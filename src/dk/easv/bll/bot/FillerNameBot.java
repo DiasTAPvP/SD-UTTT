03f6d08f62e7a740ab87f9e89898df08034ed95e
DiasTA <christofferabraham@gmail.com>
2025-03-03 13:13:53 +0100

    Added basic bot

---
+++ src/dk/easv/bll/bot/FillerNameBot.java
@@ -0,0 +1,28 @@
+package dk.easv.bll.bot;
+
+import dk.easv.bll.game.IGameState;
+import dk.easv.bll.move.IMove;
+
+public class FillerNameBot {
+    private static final String BOT_NAME="FillerBotName";
+
+
+    public IMove doMove(IGameState state) {
+
+        return null;
+    }
+
+
+
+
+
+
+    
+
+    //@Override
+    public String getBotName() {
+        return BOT_NAME;
+    }
+
+
+}
